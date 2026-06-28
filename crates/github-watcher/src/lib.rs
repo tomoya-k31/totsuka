2b3fb29a33c3fc63d5bcd3e57d66cc23e70805d1
tomoya-k31 <1076863+tomoya-k31@users.noreply.github.com>
2026-06-29 02:18:58 +0900

    feat(github-watcher): bin/lib scaffold + workspace wire-up

---
+++ crates/github-watcher/src/lib.rs
@@ -0,0 +1,22 @@
+#![forbid(unsafe_code)]
+
+use std::sync::Arc;
+use totsuka_config::Config;
+use totsuka_core::Clock;
+
+pub struct WatcherApp {
+    #[allow(dead_code)]
+    config: Arc<Config>,
+    #[allow(dead_code)]
+    clock: Arc<dyn Clock>,
+}
+
+impl WatcherApp {
+    pub fn new(config: Arc<Config>, clock: Arc<dyn Clock>) -> Self {
+        Self { config, clock }
+    }
+    pub async fn run(self) -> anyhow::Result<()> {
+        tracing::info!("github-watcher stub: nothing to do yet");
+        Ok(())
+    }
+}
